00fbbf154098aede6de2a0cfe16e7d09034d3d80
Code of K <korjon0000@gmail.com>
2024-01-21 06:07:48 -0500

    completed the score algorithm and parsing of data

--- backend/data-manipulation/polygone-zone-creation.py
+++ backend/data-manipulation/polygone-zone-creation.py
@@ -1,4 +1,4 @@
-from backend import db_connection as db
+import db_connection as db
 import test_data_manipulation as data_coordinates
 
 
@@ -44,5 +44,5 @@ if __name__ == "__main__":
     database = db.client.open_montreal
     collection = database.actes_criminels
     borough_dict = data_coordinates.get_borough_coordinates()
-    create_crime_zone(collection,borough_dict)
+    create_crime_zone(collection, borough_dict)
     db.client.close()
