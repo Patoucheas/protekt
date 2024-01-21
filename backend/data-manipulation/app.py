3f0ccae0a4bf6488675216562c78a41c83879bf4
cedparadis <cedparadis99@gmail.com>
2024-01-21 03:43:34 -0500

    Finish queries method and flask api

--- backend/data-manipulation/app.py
+++ backend/data-manipulation/app.py
@@ -1,22 +1,32 @@
 # Import the Flask class from the flask module
-from flask import Flask
-from additional_information_queries import get_additional_information as info
+from flask import Flask, jsonify
+from additional_information_queries import get_additional_information
 from backend import db_connection as db
-import jsonify
+
 
 # Create an instance of the Flask class
 app = Flask(__name__)
 
+database = db.client.open_montreal
+collection = database.actes_criminels
+
 
 # Define a route for the root URL
-@app.route("/temp")
-def home():
-    return "Server is up bitch"
+def remove_none_values(dictionary):
+    return {key: value for key, value in dictionary.items() if key is not None}
 
 
 @app.route("/additional")
 def send_addtionial_info():
-    info
+    info_dict = get_additional_information(collection)
+    # Check if info_dict is None before jsonify
+    if info_dict is not None:
+        info_dict_processed = remove_none_values(info_dict)
+        response = jsonify(info_dict_processed)
+        return response
+    else:
+        # Handle the case where info_dict is None, e.g., return an appropriate response
+        return jsonify({"error": "No additional information available"})
 
 # Run the application if this script is executed
 
